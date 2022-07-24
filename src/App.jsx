028d52e75402996c12f257b8042f28b1660dd96d
Vinothkumar Sanjeevi <vinoth.sanjeevi@sarlux.com>
2022-07-24 07:44:39 +0530

    Deploy webapp

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Product from './components/Product';
+import Home from './components/Home';
+import Item from './components/Item';
+import {Switch, Route} from 'react-router-dom'
+import Navbar from './components/Navbar';
+function App() {
+  return (
+
+    <div className="App">
+       <Navbar /> 
+   <Switch>
+   <Route exact path="/Product" component={Product} />
+    <Route exact path="/Product/:id" component={Item} />
+   </Switch>
+    </div>
+  );
+}
+
+export default App;
